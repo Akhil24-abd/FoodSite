ba2bca7acc47b7033a81ec796f7b56692457912b
Akhil24-abd <akghnekar2020@gmail.com>
2022-04-08 08:16:34 +0530

    pro3

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let navbar = document.querySelector('.header .navbar');
 let menuBtn = document.querySelector('#menu-btn');
 
 if("serviceWorker" in navigator){
-   navigator.serviceWorker.register("https://akhil24-abd.github.io/foodwebsite/sw.js")
+   navigator.serviceWorker.register("https://akhil24-abd.github.io/PWAWebApp/sw.js")
    .then(()=>{
       console.log("sw registered")
    })
